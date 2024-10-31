add8c7e25f04c9ab68757fb5fdc84b474549d040
Arthur-Quentel <quentelarthur@gmail.com>
2024-10-31 12:30:01 +0100

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,8 @@ autoreconf --install
 
 ./configure
 
-echo "Configuration complete. You now run 'make' to build the project.
+echo "Configuration complete. You now run 'make' to build the project".
+echo "You can run ./configure --<additional>-<flags> if you need specific requirements."
+echo "After finishing the build you'll have to run 'sudo make install'"
 
 
